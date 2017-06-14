4c4129de34d104951e3e2b1dc22d458d3397ae8b
rafay29 <rafaynizami@gmail.com>
2017-06-15 01:23:44 +0530

    Rename Project Add Setting in app config Remove hard coded name

---
+++ RenCs/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RenCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = System.Configuration.ConfigurationManager.AppSettings.Get("source");
+            var oldProjectName = System.Configuration.ConfigurationManager.AppSettings.Get("oldProjectName");
+            var newProjectName = System.Configuration.ConfigurationManager.AppSettings.Get("newProjectName");
+            var oldProjectNamePattern = oldProjectName + "*";
+            var newProjectNamePattern = newProjectName + "*";
+
+
+            var allSourceFolders = Directory.GetDirectories(source, oldProjectNamePattern, SearchOption.AllDirectories);
+            Console.WriteLine("Renaming folder.");
+            if (allSourceFolders.Length > 0)
+            {
+                foreach (var item in allSourceFolders)
+                {
+                    Directory.Move(item, ReplaceLastOccurrence(item, oldProjectName, newProjectName));
+                }
+            }
+
+            Console.WriteLine("Renaming cs file.");
+            var allCSFiles = Directory.GetFiles(source, "*.cs", SearchOption.AllDirectories);
+            foreach (var item in allCSFiles)
+            {
+                File.WriteAllText(item, Regex.Replace(File.ReadAllText(item), "namespace " + oldProjectNamePattern, "namespace ABC"));
+                File.WriteAllText(item, Regex.Replace(File.ReadAllText(item), "using " + oldProjectNamePattern, "using ABC"));
+            }
+
+            Console.WriteLine("Assembly info file.");
+            var allAssemblyInfoFiles = Directory.GetFiles(source, "AssemblyInfo.cs", SearchOption.AllDirectories);
+            foreach (var item in allAssemblyInfoFiles)
+            {
+                File.WriteAllText(item, Regex.Replace(File.ReadAllText(item), oldProjectNamePattern, newProjectName));
+            }
+
+            var allSourceFiles = Directory.GetFiles(source, oldProjectNamePattern, SearchOption.AllDirectories);
+            if (allSourceFiles.Length > 0)
+            {
+                foreach (var item in allSourceFiles)
+                {
+                    File.WriteAllText(item, Regex.Replace(File.ReadAllText(item), oldProjectNamePattern, newProjectName));
+                    File.Move(item, ReplaceLastOccurrence(item, oldProjectName, newProjectName));
+                }
+            }
+
+
+        }
+
+        public static string ReplaceLastOccurrence(string Source, string Find, string Replace)
+        {
+            int place = Source.LastIndexOf(Find);
+
+            if (place == -1)
+                return Source;
+
+            string result = Source.Remove(place, Find.Length).Insert(place, Replace);
+            return result;
+        }
+    }
+}
--- RenCs/Properties/AssemblyInfo.cs
+++ RenCs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication2")]
+[assembly: AssemblyTitle("RenCs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication2")]
+[assembly: AssemblyProduct("RenCs")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
